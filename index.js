265cee7335690cb4ffeacf668992092f256bd843
Mike <mikewconroy3@gmail.com>
2021-11-03 16:35:41 -0500

    commit tree dac6b79eb63a4d139ad31e359913f23301c419f4

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 16
+let num4 = 4
+let mod = num4 % num3
+
+let max = Math.max( 18,19,20);
